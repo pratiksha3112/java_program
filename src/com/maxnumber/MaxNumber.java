607c28fbd2c619509b41b0178424028fdb58e0c7
pratiksha3112 <pratiksha.mandale123@gmail.com>
2021-09-14 18:57:24 +0530

    this is my java program

---
+++ src/com/maxnumber/MaxNumber.java
@@ -0,0 +1,19 @@
+package com.maxnumber;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+
+        int x = 6;
+        int y = 9 ;
+        
+        if(x>y)     
+            System.out.println(x);
+        else
+            System.out.println(y);
+            
+             
+         
+
+    }
+}
---
+++ src/com/maxnumber/TernaryOprator.java
@@ -0,0 +1,12 @@
+package com.maxnumber;
+
+public class TernaryOprator {
+
+	public static void main(String[] args) {
+        int x = 6;
+        int y = 9 , max ;
+        max = (x>y) ? x :y ;
+        System.out.println(max);
+
+    }
+}
